ff8a8facf5cababee2a7e94050b3dc7060d25d96
Prashant Kumar <167887913+inboxprashant@users.noreply.github.com>
2024-05-21 01:44:30 +0530

    added main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+
+var typed = new Typed('.multiple-text', {
+    strings: ["Frontend Developer", "Web Developer", "Backend Developer"],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+  });
+
+
+
+
+
+  // 23
